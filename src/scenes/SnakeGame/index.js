68fdcbe5e61055c731ad8dd09b8c966d28b48cc9
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-04-03 19:00:32 +0430

    add key-press handler on useEffect

--- src/scenes/SnakeGame/index.js
+++ src/scenes/SnakeGame/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Apple from './Components/Apple';
 import Snake from './Components/Snake';
 import './style.scss'
@@ -6,9 +6,40 @@ import './style.scss'
 const SnakeGame = () => {
 
     const [snakeCells, setSnakeCells] = useState([[0, 0], [0, 5]]);
+    const [keyPress, setKeyPress] = useState("RIGHT");
+
+    const handleKeyPress = useCallback(event => {
+        let value;
+        const { keyCode } = event;
+        switch (keyCode) {
+            case 38:
+                value = 'UP';
+                break;
+            case 40:
+                value = 'DOWN';
+                break;
+            case 37:
+                value = 'LEFT';
+                break;
+            case 39:
+                value = 'RIGHT';
+                break;
+            default:
+                return;
+        }
+        setKeyPress(value)
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyPress);
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleKeyPress]);
 
     return (
         <div className="snake-game">
+            {`User Key Press: ${keyPress}`}
             <div className="display-center">
                 <div className="game-zone">
                     <Apple />
