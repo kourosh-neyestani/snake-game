1eb77f814d45249756b60a22d35db778107eaead
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-04-04 19:57:58 +0430

    fix bugs, remove duplicate checkBorders() on useEffect

--- src/scenes/SnakeGame/index.js
+++ src/scenes/SnakeGame/index.js
@@ -107,7 +107,6 @@ const SnakeGame = () => {
 
     useEffect(() => {
         return () => {
-            checkBorders()
             checkBorders();
             checkGrow();
             checkSuicide()
